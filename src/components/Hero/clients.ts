319651a76ba968902968e9bd7135fe28c7dd0475
hiigorsilva <higorscontato@gmail.com>
2024-10-30 18:03:51 -0300

    feat: create Hero Section

--- src/components/Hero/clients.ts
+++
@@ -1,7 +0,0 @@
-export const clients = [
-  { image: '/images/client-1.jpg', name: 'client-1' },
-  { image: '/images/client-2.jpg', name: 'client-2' },
-  { image: '/images/client-3.jpg', name: 'client-3' },
-  { image: '/images/client-4.jpg', name: 'client-4' },
-  { image: '/images/client-5.jpg', name: 'client-5' },
-]
---
+++ src/data/clients.ts
@@ -0,0 +1,7 @@
+export const clients = [
+  { image: '/images/hero/avatar-1.webp', name: 'client-1' },
+  { image: '/images/hero/avatar-2.webp', name: 'client-2' },
+  { image: '/images/hero/avatar-3.webp', name: 'client-3' },
+  { image: '/images/hero/avatar-4.webp', name: 'client-4' },
+  { image: '/images/hero/avatar-5.webp', name: 'client-5' },
+]
