b564714e771b13881bae4d5f3e24edbefb55c362
hiigorsilva <higorscontato@gmail.com>
2024-10-31 14:48:32 -0300

    feat: create mobile responsive Testimonials section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,6 +4,9 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      screens: {
+        xs: '405px',
+      },
       borderRadius: {
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
