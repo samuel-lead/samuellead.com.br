31e92f2c6513957ba0aee4484f855bc5b1aba73b
hiigorsilva <higorscontato@gmail.com>
2024-11-05 20:21:54 -0300

    feat: create the whatsapp link, the group link and add it to each section of the page

---
+++ src/data/contact.ts
@@ -0,0 +1,5 @@
+const numberWhatsApp = '5562999999999'
+const message = 'Olá Samuel, tenho interesse no *Venda na Primeira Conversa*.'
+
+export const linkPvWhatsApp = `https://wa.me/${numberWhatsApp}?text=${encodeURI(message)}`
+export const linkGroupWhatsApp = 'https://chat.whatsapp.com/ID_DO_GRUPO'
