e91e03ad301a73e7ed60b7996f64c7b197a3a819
hiigorsilva <higorscontato@gmail.com>
2025-01-27 23:03:38 -0300

    feat: add link to payment page and whatsapp group

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -1,5 +1,5 @@
 const numberWhatsApp = '5562999999999'
 const message = 'Olá Samuel, tenho interesse no *Venda na Primeira Conversa*.'
 
-export const linkPvWhatsApp = `https://wa.me/${numberWhatsApp}?text=${encodeURI(message)}`
-export const linkGroupWhatsApp = 'https://chat.whatsapp.com/ID_DO_GRUPO'
+export const linkPvWhatsApp = 'https://pay.kiwify.com.br/k3PZIMk'
+export const linkGroupWhatsApp = 'https://chat.whatsapp.com/KTWhlEuNNHtGMQ3AzUWkm3'
